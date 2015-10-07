788dd16d0171e82edfd0fd2f867a6890de1f4b2f
Gurpreet Singh <Guppst3r@gmail.com>
2015-10-07 02:30:40 -0400

    Recognizes Directory / Regular File without d_type

--- Virtual Machine/client/client.c
+++ Virtual Machine/client/client.c
@@ -5,11 +5,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	struct dirent *dEnt;
+	struct stat fInfo;
 	DIR *pDir;
+	char full_path[512];						//Allocate enough space to hold a directory path
 
 	if (argc != 3) // argc should be 3 for correct execution
 	{
@@ -23,31 +26,47 @@ int main(int argc, char *argv[])
 		if (pDir == NULL) 
 		{
 			printf("Cannot open directory '%s'\n", argv[1]);
-			exit(EXIT_FAILURE);
+			
 		}
 
 		while ((dEnt = readdir(pDir)) != NULL)
 		{
-
+			//Exclude itself and previous directory
 			if (strcmp(dEnt->d_name, ".") == 0 || strcmp(dEnt->d_name, "..") == 0)
 			{
 				continue;
 			}
+
+			//Obtain the current working directory and store in full_path
+			getcwd(full_path, sizeof(full_path));
+			strcat(full_path, "/");
+			strcat(full_path, argv[1]);
+			strcat(full_path, "/");
+
+			//Concatinate the name of the retrieved object with the full path for use in stat()
+			strcat(full_path, dEnt->d_name);
+
+			//Obtain information about the file
+			if (lstat(full_path, &fInfo) == -1)
+			{
+				perror(dEnt->d_name);
+				exit(EXIT_FAILURE);
+			}
+
+			//If it is directory
+			if (S_ISDIR(fInfo.st_mode))
+			{
+				printf("DIRECTORY");
+				printf("[%s]\n", dEnt->d_name);
+			}
+			else if (S_ISREG(fInfo.st_mode))	//If it is a regular file
+			{
+				printf("REGULAR FILE");
+				printf("[%s]\n", dEnt->d_name);
+			}
 			else
 			{
-				//If it is directory (d_type is always 0.... am i going to have to use stat(2) :o)
-				if ((dEnt->d_type == DT_DIR))
-				{
-					
-				}
-				else if (!(dEnt->d_type == DT_DIR))	//If it isnt a directory
-				{
-					printf("[%s]\n", dEnt->d_name);
-				}
-				else
-				{
-					exit(EXIT_FAILURE);
-				}
+				exit(EXIT_FAILURE);
 			}
 		}
 
