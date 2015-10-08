f38efcbf6c0b02ec0cb902892eeaea0a6def884b
Gurpreet Singh <Guppst3r@gmail.com>
2015-10-07 23:23:03 -0400

    Recursion works (kinda)

--- Virtual Machine/client/client.c
+++ Virtual Machine/client/client.c
@@ -7,76 +7,87 @@
 #include <unistd.h>
 #include <string.h>
 
+void traverse();
+
 int main(int argc, char *argv[])
 {
-	struct dirent *dEnt;
-	struct stat fInfo;
-	DIR *pDir;
-	char full_path[1024];									//Allocate enough space to hold a directory path
-
 	if (argc != 3) // argc should be 3 for correct execution
 	{
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		//Open the directory and store a pointer to it's location
-		pDir = opendir(argv[1]);
-
-		if (pDir == NULL) 
-		{
-			printf("Cannot open directory '%s'\n", argv[1]);
-			
-		}
-
-		while ((dEnt = readdir(pDir)) != NULL)
-		{
-			//Exclude itself and previous directory
-			if (strcmp(dEnt->d_name, ".") == 0 || strcmp(dEnt->d_name, "..") == 0)
-			{
-				continue;
-			}
-
-			//Obtain the current working directory and store in full_path
-			getcwd(full_path, sizeof(full_path));
-			strcat(full_path, "/");
-			strcat(full_path, argv[1]);
-			strcat(full_path, "/");
-
-			//Concatinate the name of the retrieved object with the full path for use in stat()
-			strcat(full_path, dEnt->d_name);
-
-			//Obtain information about the file
-			if (lstat(full_path, &fInfo) == -1)
-			{
-				perror(dEnt->d_name);
-				//Do we EXIT_FAILURE here or keep going? Keep going. 
-				continue;
-			}
-
-			//If it is directory
-			if (S_ISDIR(fInfo.st_mode))
-			{
-				printf("DIRECTORY");
-				printf("[%s]\n", dEnt->d_name);
-
-				//Enter directory and recursivly repeat process
-			}
-			else if (S_ISREG(fInfo.st_mode))	//If it is a regular file
-			{
-				printf("REGULAR FILE");
-				printf("[%s]\n", dEnt->d_name);
-				//Compute a CRC-32 checksum 
-			}
-
-		}
-
-		closedir(pDir);
-
+		traverse(argv[1]);
 		//Write a comma seperated list of files and their CRC-32 checksums to the file argv[2]
 	}
 	return 0;
 }
+
+void traverse(char* name)
+{
+	struct dirent *dEnt;
+	struct stat fInfo;
+	DIR *pDir;
+	char full_path[1024];						//Allocate enough space to hold a directory path 
+
+	getcwd(full_path, sizeof(full_path));
+	printf("[%s]", name);
+
+	//Open the directory and store a pointer to it's location
+	pDir = opendir(name);
+
+	if (pDir == NULL)
+	{
+		printf("Cannot open directory '%s'\n", name);
+	}
+
+	while ((dEnt = readdir(pDir)) != NULL)
+	{
+		//Exclude itself and previous directory
+		if (strcmp(dEnt->d_name, ".") == 0 || strcmp(dEnt->d_name, "..") == 0)
+		{
+			continue;
+		}
+
+		//Obtain the current working directory and store in full_path
+		getcwd(full_path, sizeof(full_path));
+		strcat(full_path, "/");
+		strcat(full_path, name);
+		strcat(full_path, "/");
+
+		//Concatinate the name of the retrieved object with the full path for use in stat()
+		strcat(full_path, dEnt->d_name);
+
+		//Obtain information about the file
+		if (lstat(full_path, &fInfo) == -1)
+		{
+			perror(dEnt->d_name);
+			//Do we EXIT_FAILURE here or keep going? Keep going. 
+			continue;
+		}
+
+		//If it is directory
+		if (S_ISDIR(fInfo.st_mode))
+		{
+			printf("DIRECTORY");
+			printf("[%s]\n", dEnt->d_name);
+			strcat(name, "/");
+			strcat(name, dEnt->d_name);
+
+			//Enter directory and recursivly repeat process
+			traverse(name);
+		}
+		else if (S_ISREG(fInfo.st_mode))	//If it is a regular file
+		{
+			printf("REGULAR FILE");
+			printf("[%s]\n", dEnt->d_name);
+			//Compute a CRC-32 checksum 
+		}
+
+	}
+
+	closedir(pDir);
+}//End of traverse method
 
 
 
