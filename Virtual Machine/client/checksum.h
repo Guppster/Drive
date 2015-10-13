ec16855ae156a3e7ac9d19557af87d7aa10c9807
Gurpreet Singh <Guppst3r@gmail.com>
2015-10-12 22:27:31 -0400

    Removing useless method declaration

--- Virtual Machine/client/checksum.h
+++ Virtual Machine/client/checksum.h
@@ -5,5 +5,5 @@ Description: This is the header file containing definations for the checksum.c f
 
 #include <zlib.h>
 
+//This method computes the crc checksum and returns it
 uLong computeChecksum(const Bytef*, long);
-char* readBuffer(char*, int);
