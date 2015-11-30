265b895cfa237be2f69731b4e391e9c1b790076c
Gurpreet Singh <Guppst3r@gmail.com>
2015-11-30 13:28:50 -0500

    still learning binary protocol

--- client/client.h
+++ client/client.h
@@ -12,13 +12,3 @@
 
 void sendToServer(int sockfd, char* msg, char* buffer);
 
-typedef struct{
-	unsigned int type;
-	unsigned int numSeq;
-	unsigned int length; 
-	unsigned int fileSize; 
-	unsigned int checksum; 
-	unsigned int token;
-	uint8_t filename[100];
-}msgCtrl;
-		
---
+++ common/control_message.h
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+#ifndef CTRL_MESSAGE_H
+#define CTRL_MESSAGE_H
+
+typedef struct
+{
+  uint8_t type;                
+  uint8_t numSeq;        
+  uint16_t length;           
+  uint32_t filesize;                    
+  uint32_t checksum;          
+  uint8_t token[16];
+  uint8_t filename[65511]               
+} ctrl_message;
+
+#endif
