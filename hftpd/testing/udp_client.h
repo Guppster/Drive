4062faa27a77bbce5de434cf99b754e61e79561b
Gurpreet Singh <Guppst3r@gmail.com>
2015-12-05 17:48:03 -0500

    removing test directory

--- hftpd/testing/udp_client.h
+++
@@ -1,8 +0,0 @@
-#include "udp_sockets.h"
-
-#ifndef UDP_CLIENT_H
-#define UDP_CLIENT_H
-                                                                        
-int create_client_socket(char* hostname, char* port, host* server);
-
-#endif
