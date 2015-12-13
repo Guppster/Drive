50f1e8e7666fffdf5b76bd204ac037a039659f02
Gurpreet Singh <Guppst3r@gmail.com>
2015-12-12 22:49:44 -0500

    Made directory, and opened file

--- hftpd/server.c
+++ hftpd/server.c
@@ -1,7 +1,4 @@
 #include "server.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
 //0 for no error, 1 for error (invalid token)
 message* create_ctrl_response_message(ctrl_message* request, int error, int seqNum)
@@ -20,12 +17,13 @@ message* create_ctrl_response_message(ctrl_message* request, int error, int seqN
 
 int main(int argc, char *argv[])
 {
-	//FILE *fp;
+	FILE *fp;
 	//FILE *prevfp;
-	ctrl_message* request;	 			// Client's request message
-	host client;						// Client's address
+	ctrl_message* requestctrl;	 					// Client's request message
+	data_message* requestdata;	 					// Client's data message
+	host client;									// Client's address
 	int expectedSeqNum = 0;
-	char* options[4] = { 0 };			// Declare an array of 4 options to be read in from command line
+	char* options[4] = { 0 };						// Declare an array of 4 options to be read in from command line
 	char* username = calloc(30,sizeof(char));
 	char token[17];
 	token[16] = '\0';
@@ -43,67 +41,86 @@ int main(int argc, char *argv[])
 	do
 	{
 		// Read the request message and generate the response
-		request = (ctrl_message*)receive_message(sockfd, &client);
+		requestctrl = (ctrl_message*)receive_message(sockfd, &client);
+
+		//Check if the seqNum is what it should be
+		if ((requestctrl->numSeq) != expectedSeqNum)
+		{
+			continue;
+		}
 
 		//int checksum = ntohl(request->checksum);
 		//int filesize = ntohl(request->filesize);
 
-		char filename[request->flength];
+		//Create a filename string of length read in
+		char filename[requestctrl->flength];
 
-		memcpy(filename, (char*)request->filename, request->flength);
-		memcpy(token, (char*)request->token, 16);
+		//Copy in the filename from the struct to local variable
+		memcpy(filename, (char*)requestctrl->filename, requestctrl->flength);
 
+		//Copy in the token from the struct to a local variable
+		memcpy(token, (char*)requestctrl->token, 16);
+
+		//Authenticate the read in token and retrieve a username
 		username = hdb_verify_token(dbConnection, token);
 
-		char directory[strlen(options[2]) + strlen(username) + strlen(filename) + 2];
-		
-		sprintf(directory, "%s/%s/%s", options[2], username, filename);
-
-		printf("[%s]\n", directory);
-
-		//Check if the seqNum is what it should be
-		if ((request->numSeq) != expectedSeqNum)
-		{
-			//continue;
-		}
-
 		//Send back the control message response 
-		if ((request->type == 1) && username != NULL)
+		if ((requestctrl->type == 1) && username != NULL)
 		{
-			//close previous file (why not close at end of data recieving?)
-			//fclose(prevfp);
+			//Create a directory with the passed in root directory, the user's username, and the filename + 2 for 2 '/'s
+			char abs_directory[strlen(options[2]) + strlen(username) + strlen(filename) + 2];
+
+			//Copy the outlined data into the directory field and seperate it with '/'s
+			sprintf(abs_directory, "%s/%s/%s", options[2], username, filename);
+
+			char* lastslash = strrchr(abs_directory, '/');
+
+			char directory[lastslash - abs_directory];
+
+			directory[lastslash - abs_directory] = '\0';
+
+			strncpy(directory, abs_directory, lastslash - abs_directory);
+
+			//Make a new directory and ignore errno value so do nothing if it already exists
+			mkdir(directory, 0700);
+
+			//open new file
+			fp = fopen(abs_directory, "wb");
 
 			//update metadata
 
-			//open new file
-			//fp = fopen(filename, "wb");
-
 			//ACK(seq)
-			message* respMsg = create_ctrl_response_message(request, 0, expectedSeqNum);
+			message* respMsg = create_ctrl_response_message(requestctrl, 0, expectedSeqNum);
+
+			// Send the response 
+			send_message(sockfd, respMsg, &client);
 
 			//expectedSeqNum = !expectedSeqNum
 			expectedSeqNum = 1;
 
-			// Send the response and free the memory allocated to the messages
-			send_message(sockfd, respMsg, &client);
-			free(request);
+			//Free the request and response messages
+			free(requestctrl);
 			free(respMsg);
+
+			// Read the request message and generate the response
+			requestdata = (data_message*)receive_message(sockfd, &client);
 		}
 		else
 		{
 			message* respMsg = create_ctrl_response_message(request, 0, expectedSeqNum);
 
-			// Send the response and free the memory allocated to the messages
+			// Send the response 
 			send_message(sockfd, respMsg, &client);
+
+			//Free the request and response messages
 			free(request);
 			free(respMsg);
-
 		}
-
 	} while (1);
 
 	// Close the socket
 	close(sockfd);
 
+	//Exit successfully
 	exit(EXIT_SUCCESS);
-}
+}//End of main method
