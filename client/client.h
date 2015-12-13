cb0a7b440478ca1069ced0f19aaf28ca0333b252
Gurpreet Singh <Guppst3r@gmail.com>
2015-12-13 03:28:46 -0500

    Sorted all header files (learned sort command in Vim) :)

--- client/client.h
+++ client/client.h
@@ -1,29 +1,28 @@
-
-#include <stdio.h>
-#include <string.h>
-#include <hfs.h>
-#include "../common/parse.h"
-#include "../common/networking.h"
-#include "../common/message.h"
-#include "../common/udp_client.h"
-#include "../common/udp_sockets.h"
-
-#define LISTBODYLENGTH 10000
 #define AUTHBUFFERLENGTH 50
+#define LENGTH_OF_TOKEN_TITLE 6
+#define LISTBODYLENGTH 10000
 #define LISTBUFFERLENGTH 10000
-#define TEMPSTRLENGTH 1000
 #define SIZE_OF_BYTE 8
 #define SIZE_OF_CONTROLMSG 28
-#define LENGTH_OF_TOKEN_TITLE 6
-#define SIZE_OF_DATAMSG 4
 #define SIZE_OF_DATA 1467
+#define SIZE_OF_DATAMSG 4
+#define TEMPSTRLENGTH 1000
+
+#include "../common/message.h"
+#include "../common/networking.h"
+#include "../common/parse.h"
+#include "../common/udp_client.h"
+#include "../common/udp_sockets.h"
+#include <hfs.h>
+#include <stdio.h>
+#include <string.h>
 
 
 int main (int, char*[]);
-void sendToServer(int sockfd, char* msg, char* buffer);
-void sendFiles(char* filelist, char* address, char* port, char* token, hfs_entry* listRoot);
+long getFilesize(char* filename);
 message* createCtrlMessage(char* filename, char* token, char* details[], int nextSeq, int type);
 message* createDataMessage(char* filename, int nextSeq, int alreadyReadIn, int bytesToSend);
 void getDetails(char* filename, char* details[], hfs_entry* listRoot);
-long getFilesize(char* filename);
 void readInFile(char* buffer, char* filename, int alreadyReadIn, int bytesToRead);
+void sendFiles(char* filelist, char* address, char* port, char* token, hfs_entry* listRoot);
+void sendToServer(int sockfd, char* msg, char* buffer);
--- hmds/server.h
+++ hmds/server.h
@@ -1,12 +1,11 @@
-#include <string.h>
-#include "../common/parse.h"
-#include "../common/networking.h"
-
-#define LISTBODYLENGTH 10000
-#define CREDENTIALSLENGTH 40
 #define AUTHMESSAGELENGTH 80
-#define LISTMSGLENGTH 11000
 #define BUFFERLENGTH 4096
+#define CREDENTIALSLENGTH 40
+#define LISTBODYLENGTH 10000
+#define LISTMSGLENGTH 11000
+#include "../common/networking.h"
+#include "../common/parse.h"
+#include <string.h>
 
 int wait_for_connection(int sockfd);
 void handle_connection(int connectionfd, char* hostname);
